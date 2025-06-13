15515cc24794a8b4d7abb0385eb035b4b710bbe1
Panha-VTechs <yoem.panha@vtech.one>
2025-06-13 15:09:50 +0700

    Update gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.dlna_casting_android"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.dlna_casting_android"
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.crash.afterEvaluate
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -5,19 +7,19 @@ plugins {
     alias(libs.plugins.android.library) apply false
 }
 
-//subprojects {
-//    afterEvaluate {
-//        plugins.withId("com.android.library") {
-//            configure<PublishingExtension> {
-//                publications {
-//                    create<MavenPublication>("maven") {
-//                        from(components["release"])
-//                        groupId = "com.github.yourusername"
-//                        artifactId = project.name
-//                        version = "1.0.0"
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+subprojects {
+    afterEvaluate { project ->
+        if (project.plugins.hasPlugin("com.android.library")) {
+            project.extensions.configure<PublishingExtension> {
+                publications {
+                    create<MavenPublication>("maven") {
+                        from(project.components["release"])
+                        groupId = project.findProperty("GROUP_ID") as String
+                        artifactId = project.name
+                        version = project.findProperty("VERSION_NAME") as String
+                    }
+                }
+            }
+        }
+    }
+}
