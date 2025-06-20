def8445959da99df9945309b76ccce48611bf19e
Panha-VTechs <yoem.panha@vtech.one>
2025-06-20 13:46:51 +0700

    Update connect device function to use coroutine

--- dlna-dmc/src/main/java/com/android/cast/dlna/dmc/extension/DeviceControlExtension.kt
+++ dlna-dmc/src/main/java/com/android/cast/dlna/dmc/extension/DeviceControlExtension.kt
@@ -1,73 +1,84 @@
 package com.android.cast.dlna.dmc.extension
 
+import com.android.cast.dlna.dmc.DLNACastManager
 import com.android.cast.dlna.dmc.control.DeviceControl
+import com.android.cast.dlna.dmc.control.EmptyDeviceControl
+import com.android.cast.dlna.dmc.control.OnDeviceControlListener
 import com.android.cast.dlna.dmc.control.safeCoroutineLauncher
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.withContext
+import org.fourthline.cling.model.meta.Device
+import org.fourthline.cling.support.lastchange.EventedValue
 import org.fourthline.cling.support.model.BrowseFlag
 
-suspend fun DeviceControl.setAVTransportURI(uri: String, title: String) =
+suspend fun DeviceControl.setAVTransportURIAsync(uri: String, title: String) =
     safeCoroutineLauncher { callback ->
         setAVTransportURI(uri = uri, title = title, callback = callback)
     }
 
-suspend fun DeviceControl.setNextAVTransportURI(uri: String, title: String) =
+suspend fun DeviceControl.setNextAVTransportURIAsync(uri: String, title: String) =
     safeCoroutineLauncher { callback ->
         setNextAVTransportURI(uri = uri, title = title, callback = callback)
     }
 
 
-suspend fun DeviceControl.play(speed: String = "1") = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.playAsync(speed: String = "1") = safeCoroutineLauncher { callback ->
     play(speed = speed, callback = callback)
 }
 
-suspend fun DeviceControl.stop() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.stopAsync() = safeCoroutineLauncher { callback ->
     stop(callback = callback)
 }
 
-suspend fun DeviceControl.pause() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.pauseAsync() = safeCoroutineLauncher { callback ->
     pause(callback = callback)
 }
 
-suspend fun DeviceControl.next() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.nextAsync() = safeCoroutineLauncher { callback ->
     next(callback = callback)
 }
 
-suspend fun DeviceControl.previous() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.previousAsync() = safeCoroutineLauncher { callback ->
     previous(callback = callback)
 }
 
-suspend fun DeviceControl.seek(millSeconds: Long) = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.seekAsync(millSeconds: Long) = safeCoroutineLauncher { callback ->
     seek(millSeconds = millSeconds, callback = callback)
 }
 
-suspend fun DeviceControl.getPositionInfo() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.getPositionInfoAsync() = safeCoroutineLauncher { callback ->
     getPositionInfo(callback = callback)
 }
 
-suspend fun DeviceControl.getMediaInfo() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.getMediaInfoAsync() = safeCoroutineLauncher { callback ->
     getMediaInfo(callback = callback)
 }
 
-suspend fun DeviceControl.getTransportInfo() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.getTransportInfoAsync() = safeCoroutineLauncher { callback ->
     getTransportInfo(callback = callback)
 }
 
-suspend fun DeviceControl.setVolume(volume: Int) = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.setVolumeAsync(volume: Int) = safeCoroutineLauncher { callback ->
     setVolume(volume = volume, callback = callback)
 }
 
-suspend fun DeviceControl.getVolume() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.getVolumeAsync() = safeCoroutineLauncher { callback ->
     getVolume(callback = callback)
 }
 
-suspend fun DeviceControl.setMute(mute: Boolean) = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.setMuteAsync(mute: Boolean) = safeCoroutineLauncher { callback ->
     setMute(mute = mute, callback = callback)
 }
 
-suspend fun DeviceControl.getMute() = safeCoroutineLauncher { callback ->
+suspend fun DeviceControl.getMuteAsync() = safeCoroutineLauncher { callback ->
     getMute(callback = callback)
 }
 
-suspend fun DeviceControl.browse(
+suspend fun DeviceControl.browseAsync(
     objectId: String,
     flag: BrowseFlag,
     filter: String,
@@ -84,7 +95,7 @@ suspend fun DeviceControl.browse(
     )
 }
 
-suspend fun DeviceControl.search(
+suspend fun DeviceControl.searchAsync(
     containerId: String,
     searchCriteria: String,
     filter: String,
@@ -99,4 +110,53 @@ suspend fun DeviceControl.search(
         maxResults = maxResults,
         callback = callback
     )
+}
+
+sealed interface DeviceConnectionEvent {
+    data class Connected(val device: Device<*, *, *>) : DeviceConnectionEvent
+    data class Disconnected(val device: Device<*, *, *>) : DeviceConnectionEvent
+    data class EventChanged(val event: EventedValue<*>) : DeviceConnectionEvent
+}
+
+suspend fun DLNACastManager.connectDeviceFlow(
+    device: Device<*, *, *>
+): Pair<DeviceControl, Flow<DeviceConnectionEvent>> {
+    val deviceControlReady = CompletableDeferred<DeviceControl>()
+
+    val flow = callbackFlow {
+        val listener = object : OnDeviceControlListener {
+            override fun onConnected(device: Device<*, *, *>) {
+                trySend(DeviceConnectionEvent.Connected(device))
+            }
+
+            override fun onDisconnected(device: Device<*, *, *>) {
+                trySend(DeviceConnectionEvent.Disconnected(device))
+            }
+
+            override fun onEventChanged(event: EventedValue<*>) {
+                trySend(DeviceConnectionEvent.EventChanged(event))
+            }
+        }
+
+        val control = withContext(Dispatchers.Default) {
+            runCatching {
+                connectDevice(device = device, listener = listener)
+            }.getOrElse { exception ->
+                close(exception)
+                deviceControlReady.completeExceptionally(exception)
+                return@withContext EmptyDeviceControl
+            }
+        }
+
+        if (!deviceControlReady.isCompleted) {
+            deviceControlReady.complete(control)
+        }
+
+        awaitClose {
+            disconnectDevice(device = device)
+        }
+    }
+
+    val deviceControl = deviceControlReady.await()
+    return deviceControl to flow
 }
